c98af3fcd567c11de20ebdac97916f45d270562e
guozhiheng <master@setdb.cn>
2020-01-01 22:57:50 +0800

    初始化

---
+++ src/okhttp.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace seerphp;
+
+/**
+ * Description of okhttp
+ *
+ * @author jobsz
+ */
+class okhttp {
+    //put your code here
+    public function send(){
+        echo "hello this my frist demo v1.0";
+    }
+    
+}
